ecd5572a7772aaa6ea39ef622d081fdfa87a1330
vahe-vhuk <vahevahe870@gmail.com>
2023-08-10 13:55:39 +0400

    test

--- day1/insertion_sort.cpp
+++ day1/insertion_sort.cpp
@@ -18,7 +18,7 @@ void insertion_sort(std::vector<T>& vec)
     for(size_t i = 1; i < n; ++i) {
         size_t j = i - 1;
         T val = vec[i];
-        while (vec[j] > vec[j + 1]) {
+        while (j >= 0 && vec[j] > vec[j + 1]) {
             swap(vec[j], vec[j + 1]);
             --j;
         }
